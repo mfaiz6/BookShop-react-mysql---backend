f564143166eb6f1013b91b760608ad64d1362b6e
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2022-10-20 19:46:28 +0530

    Use express middleware for backend connection with db, set endpoints to get books and to add books to mysql db

--- index.js
+++ index.js
@@ -10,18 +10,37 @@ const db = mysql.createConnection({
     database:"test"
 })
 
+//for auth error
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY process.env.DB_PASS;
+
+//middleware
+app.use(express.json())
+
 app.get("/", (req, res) => {
     res.json("Hello, this is the backend")
 })
 
-//for auth error
-// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY process.env.DB_PASS;
+
 
 app.get("/books", (req, res) => {
     const q = "SELECT * FROM books"
     db.query(q, (err, data) => {
         if(err) return res.json(err)
         return res.json(data)
+    })
+})
+
+app.post("/books", (req, res) => {
+    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover,
+    ]
+
+    db.query(q,[values], (err, data) => {
+        if(err) return res.json(err)
+        return res.json("Book added successfully!")
     })
 })
 
